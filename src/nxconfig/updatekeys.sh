f3ba9d23647e9859bf8813119458884505c064a7
Leah <leah.nixdev@gmail.com>
2025-09-07 18:33:30 +0200

    Fix updatekeys script

--- src/nxconfig/updatekeys.sh
+++ src/nxconfig/updatekeys.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 echo "# Checking files at path $PWD"
 find . -type f -print0 | while IFS= read -r -d '' file; do
-  if [[ "$(echo "$file" | grep .git)" = "" ]]; then
+  if [[ "$file" != *"/.git/"* ]]; then
     if sops -d "$file" >/dev/null 2>&1; then
         echo "++ Updating keys for encrypted file: $file"
         sops updatekeys --yes "$file"
