7ab233709741ba89046a79ebff3639cb3079a6e2
Leah <leah.nixdev@gmail.com>
2025-09-10 18:34:04 +0200

    Fix git-credential-manager init

--- src/common/modules/home/git/git-credential-manager/git-credential-manager.nix
+++ src/common/modules/home/git/git-credential-manager/git-credential-manager.nix
@@ -27,7 +27,7 @@ args@{
       home.file."${config.xdg.configHome}/fish-init/60-check-pass.fish".text = (
         if self.user.gpg != null then
           ''
-            if ! test -d "${self.user.home}/.password-store"
+            if ! test -d "${self.user.home}/.password-store"; or test -z "(ls -A "${self.user.home}/.password-store" 2>/dev/null)"
               echo "Init pass for gpg key: ${self.user.gpg}"
               pass init ${self.user.gpg}
             end
