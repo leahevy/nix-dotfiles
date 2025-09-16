38fcb88dd7f91abb178c0ef7a5028cc1b88760a2
Leah <leah.nixdev@gmail.com>
2025-09-16 08:18:10 +0200

    Fix qutebrowser with correct overlay usage

--- src/common/modules/home/browser/qutebrowser/qutebrowser.nix
+++ src/common/modules/home/browser/qutebrowser/qutebrowser.nix
@@ -11,19 +11,43 @@ args@{
 {
   name = "qutebrowser";
 
+  unfree = [ "widevine-cdm" ];
+
   configuration =
     context@{ config, options, ... }:
-    {
-      nixpkgs.overlays = [
-        (final: prev: {
-          qutebrowser = prev.qutebrowser.override { enableWideVine = true; };
-        })
-      ];
-
-      home = {
-        packages = with pkgs; [
-          qutebrowser
+    let
+      customPkgs = self.pkgs-unstable {
+        overlays = [
+          (final: prev: {
+            qutebrowser =
+              let
+                qbWithWideVine = prev.qutebrowser.override {
+                  enableWideVine = true;
+                };
+              in
+              qbWithWideVine.overrideAttrs (oldAttrs: {
+                propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
+                  final.python3Packages.pyqt6
+                ];
+                buildInputs = oldAttrs.buildInputs ++ [
+                  prev.curl
+                ];
+                nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+                  prev.makeWrapper
+                ];
+                postInstall = oldAttrs.postInstall or "" + ''
+                  wrapProgram $out/bin/qutebrowser \
+                    --prefix LD_LIBRARY_PATH : "${prev.curl.out}/lib"
+                '';
+              });
+          })
         ];
+      };
+    in
+    {
+      programs.qutebrowser = {
+        enable = true;
+        package = customPkgs.qutebrowser;
       };
 
       home.persistence."${self.persist}" = {
