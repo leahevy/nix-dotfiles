9d22fcf010eab2783e6bd425b8a7ab2587865190
Leah <leah.nixdev@gmail.com>
2025-10-04 18:18:26 +0200

    Make intel graphics driver optional

--- src/linux/modules/system/graphics/opengl/opengl.nix
+++ src/linux/modules/system/graphics/opengl/opengl.nix
@@ -11,17 +11,28 @@ args@{
 {
   name = "opengl";
 
+  defaults = {
+    withIntel = false;
+  };
+
   configuration =
     context@{ config, options, ... }:
     {
       hardware.graphics = {
         enable = true;
         enable32Bit = true;
-        extraPackages = with pkgs; [
-          intel-media-driver
-          vaapiIntel
-          vaapiVdpau
-          libvdpau-va-gl
+        extraPackages =
+          with pkgs;
+          [
+            vaapiVdpau
+            libvdpau-va-gl
+          ]
+          ++ lib.optionals self.settings.withIntel [
+            intel-media-driver
+            vaapiIntel
+          ];
+        extraPackages32 = with pkgs.pkgsi686Linux; [
+          libvdpau
         ];
       };
     };
