1c0a6b7477f0343dd131773c5d8a5ac11dc23a06
Leah <leah.nixdev@gmail.com>
2025-10-06 07:57:58 +0200

    Resize mako

--- src/linux/modules/home/desktop-modules/mako/mako.nix
+++ src/linux/modules/home/desktop-modules/mako/mako.nix
@@ -18,14 +18,14 @@ args@{
         enable = true;
 
         settings = {
-          font = lib.mkForce "monospace 11";
+          font = lib.mkForce "monospace 13";
           background-color = lib.mkForce "#0a0a0fee";
           text-color = lib.mkForce "#ffffff";
           border-color = lib.mkForce "#ffffff26";
           progress-color = lib.mkForce "#ffffff";
 
-          width = 300;
-          height = 150;
+          width = 400;
+          height = 200;
           margin = "8";
           padding = "20";
           border-size = 1;
