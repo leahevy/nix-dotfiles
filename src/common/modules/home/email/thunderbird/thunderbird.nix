19569336c934fe0d454788e9c139e3f66e1893e9
Leah <leah.nixdev@gmail.com>
2025-10-06 07:57:43 +0200

    Fix thunderbird window rules

--- src/common/modules/home/email/thunderbird/thunderbird.nix
+++ src/common/modules/home/email/thunderbird/thunderbird.nix
@@ -40,17 +40,22 @@ args@{
 
           window-rules = [
             {
-              matches = [
-                {
-                  app-id = "thunderbird";
-                  title = "^(?!.*(Reminder|Calendar|Event|Task|Address Book|Preferences|Options|Settings)).*";
-                }
-              ];
+              matches = [ { app-id = "thunderbird"; } ];
               min-width = 1500;
               min-height = 800;
               open-on-workspace = "scratch";
               open-floating = true;
               open-focused = false;
+            }
+            {
+              matches = [
+                {
+                  app-id = "thunderbird";
+                  title = ".*(Reminder|Calendar|Event|Task|Address Book|Preferences|Options|Settings).*";
+                }
+              ];
+              open-on-workspace = "nonexistent";
+              open-focused = true;
             }
             {
               matches = [ { app-id = "thunderbird"; } ];
