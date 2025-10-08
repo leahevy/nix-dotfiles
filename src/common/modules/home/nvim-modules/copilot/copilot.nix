3f284c43b7edcb5c99506e7e688a4e824ff323d9
Leah <leah.nixdev@gmail.com>
2025-10-08 06:56:21 +0200

    Add keybinding to toggle Copilot

--- src/common/modules/home/nvim-modules/copilot/copilot.nix
+++ src/common/modules/home/nvim-modules/copilot/copilot.nix
@@ -22,6 +22,26 @@ args@{
         extraPlugins = with pkgs.vimPlugins; [
           copilot-vim
         ];
+
+        keymaps = [
+          {
+            mode = "n";
+            key = "<leader>cq";
+            action = ":lua local status = vim.fn.execute('Copilot status'); if string.match(status, 'Ready') then print('Disabling Copilot...'); vim.cmd('Copilot disable') else print('Enabling Copilot...'); vim.cmd('Copilot enable') end<CR>";
+            options = {
+              desc = "Toggle Copilot";
+              silent = false;
+            };
+          }
+        ];
+
+        plugins.which-key.settings.spec = lib.mkIf (self.common.isModuleEnabled "nvim-modules.which-key") [
+          {
+            __unkeyed-1 = "<leader>cq";
+            desc = "Toggle Copilot";
+            icon = "✈️";
+          }
+        ];
       };
 
       home.persistence."${self.persist}" = {
