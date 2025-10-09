9303b6f2a3438da96a81d8e61e168a55ae4896d1
Leah <leah.nixdev@gmail.com>
2025-10-09 15:55:26 +0200

    Configure many more which-key bindings for programming languages with autocmds

--- src/common/modules/home/nvim-modules/which-key/which-key.nix
+++ src/common/modules/home/nvim-modules/which-key/which-key.nix
@@ -8,6 +8,32 @@ args@{
   self,
   ...
 }:
+let
+  programmingFileTypes = [
+    "c"
+    "cpp"
+    "h"
+    "hpp"
+    "python"
+    "javascript"
+    "typescript"
+    "jsx"
+    "tsx"
+    "java"
+    "rust"
+    "go"
+    "php"
+    "ruby"
+    "perl"
+    "lua"
+    "vim"
+    "sh"
+    "bash"
+    "zsh"
+    "fish"
+    "nix"
+  ];
+in
 {
   name = "which-key";
 
@@ -80,8 +106,59 @@ args@{
               group = "buffer";
             }
             {
-              __unkeyed-1 = "<leader>t";
-              desc = "Toggle NvimTree";
+              __unkeyed-1 = "#";
+              desc = "Search word backward";
+              mode = "v";
+            }
+            {
+              __unkeyed-1 = "*";
+              desc = "Search word forward";
+              mode = "v";
+            }
+            {
+              __unkeyed-1 = "@";
+              desc = "Execute macro";
+              mode = "v";
+            }
+            {
+              __unkeyed-1 = "<C-B>";
+              desc = "Smooth page up";
+              mode = "v";
+            }
+            {
+              __unkeyed-1 = "<C-D>";
+              desc = "Smooth half page down";
+              mode = "v";
+            }
+            {
+              __unkeyed-1 = "<C-F>";
+              desc = "Smooth page down";
+              mode = "v";
+            }
+            {
+              __unkeyed-1 = "<C-U>";
+              desc = "Smooth half page up";
+              mode = "v";
+            }
+            {
+              __unkeyed-1 = "<PageDown>";
+              desc = "Smooth page down";
+              mode = "v";
+            }
+            {
+              __unkeyed-1 = "<PageUp>";
+              desc = "Smooth page up";
+              mode = "v";
+            }
+            {
+              __unkeyed-1 = "<S-Down>";
+              desc = "Smooth shift down";
+              mode = "v";
+            }
+            {
+              __unkeyed-1 = "<S-Up>";
+              desc = "Smooth shift up";
+              mode = "v";
             }
           ];
 
@@ -93,9 +170,57 @@ args@{
             ];
           };
 
+          keys = {
+            scroll_down = "<C-PageDown>";
+            scroll_up = "<C-PageUp>";
+          };
+
           show_help = self.settings.showHelp;
           show_keys = self.settings.showKeys;
         };
       };
+
+      programs.nixvim.autoCmd = [
+        {
+          event = [ "FileType" ];
+          pattern = programmingFileTypes;
+          callback.__raw = ''
+            function()
+              local ft = vim.bo.filetype
+              if ft ~= "vimwiki" then
+                require("which-key").add({
+                  { "[[", desc = "Previous function/class", buffer = 0 },
+                  { "]]", desc = "Next function/class", buffer = 0 },
+                })
+              end
+            end
+          '';
+        }
+        {
+          event = [ "FileType" ];
+          pattern = [ "python" ];
+          callback.__raw = ''
+            function()
+              require("which-key").add({
+                { "[m", desc = "Previous method", buffer = 0 },
+                { "]m", desc = "Next method", buffer = 0 },
+                { "[M", desc = "Previous class/method (context)", buffer = 0 },
+                { "]M", desc = "Next class/method (context)", buffer = 0 },
+                { "[]", desc = "Previous class/function end", buffer = 0 },
+                { "][", desc = "Next class/function end", buffer = 0 },
+              })
+            end
+          '';
+        }
+        {
+          event = [ "BufLeave" ];
+          pattern = programmingFileTypes;
+          callback.__raw = ''
+            function()
+              pcall(require("which-key").clear, { buffer = 0 })
+            end
+          '';
+        }
+      ];
     };
 }
