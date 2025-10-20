57ade2d42afec0a57766a40ce6565896021f5b6e
Leah <leah.nixdev@gmail.com>
2025-10-20 18:10:51 +0200

    nx-update only updates stable nix-pkgs when no input is given

--- scripts/deployment/update.sh
+++ scripts/deployment/update.sh
@@ -5,13 +5,23 @@ source "$(dirname "${BASH_SOURCE[0]}")/../utils/pre-check.sh"
 deployment_script_setup "update"
 check_deployment_conflicts "update"
 
-echo -e "Updating flake of main repository ${WHITE}(.config/nx/nxcore)${RESET}..."
-nix flake update "$@" || true
+if [[ $# -eq 0 ]]; then
+    echo -e "Updating nixpkgs input of main repository ${WHITE}(.config/nx/nxcore)${RESET}..."
+    nix flake update nixpkgs || true
+else
+    echo -e "Updating flake of main repository ${WHITE}(.config/nx/nxcore)${RESET}..."
+    nix flake update "$@" || true
+fi
 
 if [[ -d "$CONFIG_DIR/.git" ]]; then
     echo
-    echo -e "Updating flake of config repository ${WHITE}(.config/nx/nxconfig)${RESET}..."
-    (cd "$CONFIG_DIR" && nix flake update "$@" || true)
+    if [[ $# -eq 0 ]]; then
+        echo -e "Updating nixpkgs input of config repository ${WHITE}(.config/nx/nxconfig)${RESET}..."
+        (cd "$CONFIG_DIR" && nix flake update nixpkgs || true)
+    else
+        echo -e "Updating flake of config repository ${WHITE}(.config/nx/nxconfig)${RESET}..."
+        (cd "$CONFIG_DIR" && nix flake update "$@" || true)
+    fi
 else
     echo
     echo -e "${YELLOW}Warning: Config directory does not exist or is no directory.${RESET}"
