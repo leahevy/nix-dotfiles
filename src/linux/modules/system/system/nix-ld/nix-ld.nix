e16b21a8da7f35a7e81f25fa8c6a02bb2b9e656a
Leah <leah.nixdev@gmail.com>
2025-10-25 14:36:33 +0200

    Add valid default libraries to nix-ld configuration module

--- src/linux/modules/system/system/nix-ld/nix-ld.nix
+++ src/linux/modules/system/system/nix-ld/nix-ld.nix
@@ -16,16 +16,50 @@ args@{
   namespace = "system";
 
   defaults = {
-    baseLibraries = [ ];
+    baseLibraries = with args.pkgs; [
+      stdenv.cc.cc.lib
+      zlib
+      glib
+      dbus
+      expat
+      krb5
+      nss
+      nspr
+    ];
+    desktopLibraries = with args.pkgs; [
+      xorg.libX11
+      xorg.libXrandr
+      xorg.libXcomposite
+      xorg.libXdamage
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXrender
+      xorg.libXtst
+      xorg.libXau
+      xorg.libXdmcp
+      xorg.libxcb
+      libGL
+      libdrm
+      mesa
+      wayland
+      libxkbcommon
+      freetype
+      fontconfig
+      alsa-lib
+    ];
     additionalLibraries = [ ];
   };
 
   configuration =
     context@{ config, options, ... }:
+    let
+      hasDesktop = self.host.settings.system.desktop != null;
+      desktopLibs = if hasDesktop then self.settings.desktopLibraries else [ ];
+    in
     {
       # nix-ld is already enabled through build modules.
       programs.nix-ld = {
-        libraries = self.settings.baseLibraries ++ self.settings.additionalLibraries;
+        libraries = self.settings.baseLibraries ++ desktopLibs ++ self.settings.additionalLibraries;
       };
     };
 }
