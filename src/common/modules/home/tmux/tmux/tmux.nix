ce6da33874b0bb8c4cc09f74b792eb6ce1b3e386
Leah <leah.nixdev@gmail.com>
2025-10-27 16:58:02 +0100

    Configure additional tmux keybinds

--- src/common/modules/home/tmux/tmux/tmux.nix
+++ src/common/modules/home/tmux/tmux/tmux.nix
@@ -161,6 +161,24 @@ args@{
           bind-key C-a send-prefix
 
           bind-key 0 select-window -t :10
+          bind-key - select-window -t :11
+          bind-key = select-window -t :12
+
+          bind-key -n M-1 select-window -t :1
+          bind-key -n M-2 select-window -t :2
+          bind-key -n M-3 select-window -t :3
+          bind-key -n M-4 select-window -t :4
+          bind-key -n M-5 select-window -t :5
+          bind-key -n M-6 select-window -t :6
+          bind-key -n M-7 select-window -t :7
+          bind-key -n M-8 select-window -t :8
+          bind-key -n M-9 select-window -t :9
+          bind-key -n M-0 select-window -t :10
+          bind-key -n M-- select-window -t :11
+          bind-key -n M-= select-window -t :12
+
+          bind-key -n C-S-Left previous-window
+          bind-key -n C-S-Right next-window
 
           bind -r H resize-pane -L 10
           bind -r J resize-pane -D 10
@@ -203,8 +221,8 @@ args@{
           bind \; command-prompt
           bind : last-pane
 
-          bind-key -n C-S-Left swap-window -t -1 \; select-window -t -1
-          bind-key -n C-S-Right swap-window -t +1 \; select-window -t +1
+          bind-key -n M-S-Left swap-window -t -1 \; select-window -t -1
+          bind-key -n M-S-Right swap-window -t +1 \; select-window -t +1
         '';
 
         ".config/tmux/25-vim-tmux-navigator.conf".text = ''
