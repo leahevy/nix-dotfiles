c3419e96d3dba86a24a499634ba0262319d79d93
Leah <leah.nixdev@gmail.com>
2025-10-28 17:19:20 +0100

    Add tailscale module

---
+++ src/linux/modules/system/networking/tailscale/tailscale.nix
@@ -0,0 +1,77 @@
+args@{
+  lib,
+  pkgs,
+  pkgs-unstable,
+  funcs,
+  helpers,
+  defs,
+  self,
+  ...
+}:
+{
+  name = "tailscale";
+
+  group = "networking";
+  input = "linux";
+  namespace = "system";
+
+  defaults = {
+    openFirewall = true;
+    subnetRoutes = [ ];
+    withTaildrop = false;
+    exitNode = false;
+    acceptRoutes = false;
+  };
+
+  configuration =
+    context@{ config, options, ... }:
+    let
+      isServer = self.settings.exitNode || self.settings.subnetRoutes != [ ];
+      isClient = self.settings.acceptRoutes;
+
+      useRoutingFeatures =
+        if isServer && isClient then
+          "both"
+        else if isServer then
+          "server"
+        else if isClient then
+          "client"
+        else
+          "none";
+
+      extraUpFlags = [
+        "--accept-routes"
+        "--advertise-tags=tag:${self.host.hostname}"
+      ]
+      ++ lib.optionals self.settings.exitNode [
+        "--advertise-exit-node"
+      ]
+      ++ lib.optionals (self.settings.subnetRoutes != [ ]) [
+        "--advertise-routes=${lib.concatStringsSep "," self.settings.subnetRoutes}"
+      ];
+    in
+    {
+      sops.secrets.tailscale-auth-key = {
+        format = "binary";
+        sopsFile = self.profile.secretsPath "tailscale-auth-key";
+        mode = "0400";
+        owner = "root";
+        group = "root";
+      };
+
+      services.tailscale = {
+        enable = true;
+        authKeyFile = config.sops.secrets.tailscale-auth-key.path;
+        useRoutingFeatures = useRoutingFeatures;
+        extraUpFlags = extraUpFlags;
+        disableTaildrop = !self.settings.withTaildrop;
+        openFirewall = self.settings.openFirewall;
+      };
+
+      environment.persistence."${self.persist}" = {
+        directories = [
+          "/var/lib/tailscale"
+        ];
+      };
+    };
+}
