d44d872ea2ee743701f077c27bb179a6294805c3
Leah <leah.nixdev@gmail.com>
2025-11-03 15:38:51 +0100

    Fix transparency for end of buffer

--- src/common/modules/home/nvim-modules/transparency/transparency.nix
+++ src/common/modules/home/nvim-modules/transparency/transparency.nix
@@ -27,6 +27,7 @@ args@{
           vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
           vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
           vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+          vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
           vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
           vim.api.nvim_set_hl(0, "LineNrAbove", { bg = "none" })
