96933d0d80272f4fa77fdb910facfea7db3f3e4b
Leah <leah.nixdev@gmail.com>
2025-11-06 16:06:07 +0100

    Add additional treesitter grammars

--- src/common/modules/home/nvim-modules/treesitter/treesitter.nix
+++ src/common/modules/home/nvim-modules/treesitter/treesitter.nix
@@ -20,11 +20,23 @@ args@{
     enableIndentation = true;
     enableFolding = true;
     grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      asm
+      awk
       bash
+      bibtex
       c
+      c_sharp
+      caddy
+      cairo
       cpp
       css
+      csv
+      cmake
+      desktop
+      devicetree
+      diff
       dockerfile
+      editorconfig
       fish
       git_config
       git_rebase
@@ -32,21 +44,70 @@ args@{
       gitcommit
       gitignore
       go
+      glsl
+      gnuplot
+      gpg
+      graphql
+      groovy
       html
+      haskell
+      haskell_persistent
+      http
+      jq
       javascript
+      jinja
+      jinja_inline
       json
+      jsonc
+      kdl
+      idris
       lua
+      make
+      matlab
+      muttrc
       markdown
       markdown_inline
+      mermaid
+      nasm
       nix
+      nginx
+      nu
+      objc
+      passwd
+      pem
+      powershell
+      printf
+      properties
+      proto
       python
+      pymanifest
+      r
+      readline
+      regex
+      requirements
       rust
+      ruby
+      ql
+      scss
+      sql
+      strace
+      swift
+      ssh_config
+      solidity
+      scala
+      tmux
+      turtle
+      terraform
       toml
       typescript
+      typespec
       vim
       vimdoc
       yaml
       latex
+      udev
+      xml
+      xresources
     ];
     extraGrammars = [ ];
   };
