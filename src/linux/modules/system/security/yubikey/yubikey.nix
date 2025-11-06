f3f1ecd63a23ca4d85764c87e8b54c2b4329e982
Leah <leah.nixdev@gmail.com>
2025-11-06 18:57:38 +0100

    Add yubikey module

---
+++ src/linux/modules/system/security/yubikey/yubikey.nix
@@ -0,0 +1,65 @@
+args@{
+  lib,
+  pkgs,
+  pkgs-unstable,
+  funcs,
+  helpers,
+  defs,
+  self,
+  ...
+}:
+{
+  name = "yubikey";
+
+  group = "security";
+  input = "linux";
+  namespace = "system";
+
+  settings = {
+    modelId = null;
+    lockSessionOnUnplug = false;
+    enableU2fAuth = false;
+    useU2fAuthForSudo = true;
+    useU2fAuthForLogin = true;
+  };
+
+  configuration =
+    context@{ config, options, ... }:
+    {
+      services.pcscd.enable = true;
+
+      services.udev.packages = [ pkgs.yubikey-personalization ];
+
+      environment.systemPackages = with pkgs; [
+        yubikey-personalization
+        yubikey-manager
+        libfido2
+        pam_u2f
+        pamtester
+      ];
+
+      services.udev.extraRules =
+        lib.mkIf (self.settings.modelId != null && self.settings.lockSessionOnUnplug)
+          ''
+            ACTION=="remove",\
+             SUBSYSTEM=="usb",\
+             ENV{DEVTYPE}=="usb_device",\
+             ENV{PRODUCT}=="1050/${lib.removePrefix "0" self.settings.modelId}/*",\
+             RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+          '';
+
+      sops.secrets.yubikey-u2f-keys = lib.mkIf self.settings.enableU2fAuth {
+        format = "binary";
+        sopsFile = self.config.secretsPath "yubikey-u2f-keys";
+        mode = "0644";
+      };
+
+      security.pam.u2f.settings.authfile =
+        lib.mkIf self.settings.enableU2fAuth config.sops.secrets.yubikey-u2f-keys.path;
+
+      security.pam.services = lib.mkIf self.settings.enableU2fAuth {
+        sudo.u2fAuth = self.settings.useU2fAuthForSudo;
+        login.u2fAuth = self.settings.useU2fAuthForLogin;
+      };
+    };
+}
