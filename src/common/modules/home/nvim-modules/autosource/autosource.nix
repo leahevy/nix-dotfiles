3f268d7b9ddf117a253ae822c87c1ad8a3b905eb
Leah <leah.nixdev@gmail.com>
2025-11-10 17:55:49 +0100

    Add autosource plugin

---
+++ src/common/modules/home/nvim-modules/autosource/autosource.nix
@@ -0,0 +1,82 @@
+args@{
+  lib,
+  pkgs,
+  pkgs-unstable,
+  funcs,
+  helpers,
+  defs,
+  self,
+  ...
+}:
+{
+  name = "autosource";
+
+  group = "nvim-modules";
+  input = "common";
+  namespace = "home";
+
+  settings = {
+    promptForChangedFile = true;
+    promptForNewFile = true;
+    approveOnSave = false;
+  };
+
+  configuration =
+    context@{ config, options, ... }:
+    {
+      programs.nixvim = {
+        plugins.autosource = {
+          enable = true;
+          autoLoad = true;
+          settings = {
+            prompt_for_changed_file = if self.settings.promptForChangedFile then 1 else 0;
+            prompt_for_new_file = if self.settings.promptForNewFile then 1 else 0;
+          };
+        };
+      };
+
+      home.file.".config/nvim-init/05-autosource-config.lua".text = ''
+        vim.g.autosource_approve_on_save = ${if self.settings.approveOnSave then "1" else "0"}
+
+        vim.g.autosource_disable_autocmd = 1
+
+        local autosource_ready = false
+
+        local augroup = vim.api.nvim_create_augroup('custom_autosource', { clear = true })
+
+        vim.api.nvim_create_autocmd({'BufReadPost', 'BufNewFile'}, {
+          group = augroup,
+          callback = function(event)
+            if not autosource_ready or vim.g.SessionLoad then
+              return
+            end
+
+            local filepath = event.file
+            if filepath == "" or not vim.fn.filereadable(filepath) then
+              return
+            end
+
+            pcall(vim.fn.AutoSource, vim.fn.expand('%:p:h'))
+          end
+        })
+
+        vim.api.nvim_create_autocmd('VimEnter', {
+          once = true,
+          callback = function()
+            vim.defer_fn(function()
+              autosource_ready = true
+              if vim.fn.expand('%') ~= "" then
+                pcall(vim.fn.AutoSource, vim.fn.expand('%:p:h'))
+              end
+            end, 500)
+          end
+        })
+      '';
+
+      home.persistence."${self.persist}" = {
+        directories = [
+          ".autosource_hashes"
+        ];
+      };
+    };
+}
--- src/common/modules/home/nvim/nixvim/nixvim.nix
+++ src/common/modules/home/nvim/nixvim/nixvim.nix
@@ -63,6 +63,7 @@ args@{
         codewindow = true;
         notify = true;
         trouble = true;
+        autosource = true;
       };
     };
   };
