90e32881fdc153556f3f804e651e91718dcac3fc
Leah <leah.nixdev@gmail.com>
2025-12-03 15:24:29 +0100

    Fix blinking cursors autocommands

--- src/common/modules/home/nvim/nixvim/nixvim.nix
+++ src/common/modules/home/nvim/nixvim/nixvim.nix
@@ -461,21 +461,48 @@ args@{
                 _G.nx_modules["10-blinking-cursor"] = function()
                   vim.opt.guicursor = ""
 
-                  vim.api.nvim_create_autocmd({"VimEnter"}, {
-                    callback = function()
+                  local function update_cursor()
+                    local mode = vim.fn.mode()
+                    if mode == 'i' or mode == 'ic' or mode == 'ix' then
+                      io.write("\27[5 q")
+                    elseif mode == 'R' or mode == 'Rc' or mode == 'Rx' or mode == 'Rv' then
+                      io.write("\27[3 q")
+                    elseif mode == 'c' or mode == 'cv' or mode == 'ce' then
+                      io.write("\27[5 q")
+                    elseif mode == 't' then
+                      io.write("\27[5 q")
+                    else
                       io.write("\27[1 q")
                     end
-                  })
+                  end
 
-                  vim.api.nvim_create_autocmd({"InsertEnter"}, {
+                  vim.api.nvim_create_autocmd({"InsertEnter", "CmdlineEnter", "TermEnter"}, {
                     callback = function()
                       io.write("\27[5 q")
                     end
                   })
 
-                  vim.api.nvim_create_autocmd({"InsertLeave"}, {
+                  vim.api.nvim_create_autocmd({"InsertLeave", "CmdlineLeave"}, {
                     callback = function()
                       io.write("\27[1 q")
+                    end
+                  })
+
+                  vim.api.nvim_create_autocmd({"TermLeave"}, {
+                    callback = function()
+                      vim.defer_fn(update_cursor, 10)
+                    end
+                  })
+
+                  vim.api.nvim_create_autocmd({"VimEnter", "BufEnter", "WinEnter"}, {
+                    callback = function()
+                      vim.defer_fn(update_cursor, 10)
+                    end
+                  })
+
+                  vim.api.nvim_create_autocmd({"FocusGained"}, {
+                    callback = function()
+                      vim.defer_fn(update_cursor, 50)
                     end
                   })
                 end
