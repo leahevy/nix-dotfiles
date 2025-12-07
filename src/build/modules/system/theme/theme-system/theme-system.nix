7ce40914cc900ccb3c2be4f89aaad049746194ad
Leah <leah.nixdev@gmail.com>
2025-12-07 11:37:58 +0100

    Simplify themes as they are now solely defined in the home namespace (but the allowed themes are still listed in an enum in nixOS and standalone types)

--- src/build/modules/system/theme/theme-system/theme-system.nix
+++ src/build/modules/system/theme/theme-system/theme-system.nix
@@ -8,13 +8,6 @@ args@{
   self,
   ...
 }:
-let
-  activeTheme =
-    if (self.host.settings.theme or null) != null then
-      self.host.settings.theme
-    else
-      self.variables.defaultTheme;
-in
 {
   name = "theme-system";
   group = "theme";
@@ -23,8 +16,8 @@ in
 
   submodules = {
     themes = {
-      themes = {
-        ${activeTheme} = true;
+      base = {
+        base = true;
       };
     };
   };
