049689bc6be2d1b02ef9e15004e9e8804617dbcd
Leah <leah.nixdev@gmail.com>
2025-12-14 11:29:05 +0100

    Fix bitwarden-cli for impermanence setup: 'Bitwarden' and 'Bitwarden CLI' (with a space) cannot be both persisted according to the bindMount* systemd services implementation

--- src/common/modules/home/passwords/bitwarden/bitwarden.nix
+++ src/common/modules/home/passwords/bitwarden/bitwarden.nix
@@ -17,16 +17,31 @@ args@{
 
   configuration =
     context@{ config, options, ... }:
+    let
+      customPkgs = self.pkgs {
+        overlays = [
+          (final: prev: {
+            bitwarden-cli = prev.bitwarden-cli.overrideAttrs (oldAttrs: {
+              nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ prev.makeWrapper ];
+              postInstall = (oldAttrs.postInstall or "") + ''
+                wrapProgram $out/bin/bw \
+                  --set BITWARDENCLI_APPDATA_DIR "${config.home.homeDirectory}/.config/Bitwarden-CLI"
+              '';
+            });
+          })
+        ];
+      };
+    in
     {
-      home.packages = with pkgs; [
-        bitwarden
-        bitwarden-cli
+      home.packages = [
+        pkgs.bitwarden
+        customPkgs.bitwarden-cli
       ];
 
       home.persistence."${self.persist}" = {
         directories = [
           ".config/Bitwarden"
-          ".config/Bitwarden CLI"
+          ".config/Bitwarden-CLI"
         ];
       };
     };
