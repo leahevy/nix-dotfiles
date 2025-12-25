a9f13cf5f959d8b8721608676b4bef2cb67280f0
Leah <leah.nixdev@gmail.com>
2025-12-25 23:07:02 +0100

    Configure new nix build dir due to 25.11 changes

--- src/linux/modules/system/system/tmp/tmp.nix
+++ src/linux/modules/system/system/tmp/tmp.nix
@@ -21,10 +21,11 @@ args@{
       boot.tmp.useTmpfs = true;
       boot.tmp.tmpfsSize = self.host.settings.system.tmpSize;
 
-      nix.settings.build-dir = "/var/tmp";
+      nix.settings.build-dir = "/var/nix-builds";
 
       systemd.tmpfiles.rules = [
         "d /var/tmp 1777 root root -"
+        "d /var/nix-builds 0755 root root -"
       ];
     };
 }
