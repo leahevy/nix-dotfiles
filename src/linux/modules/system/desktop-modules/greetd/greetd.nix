8ffc873bde63b333968f4f90406e5acfe8083443
Leah <leah.nixdev@gmail.com>
2025-12-26 11:11:06 +0100

    Start uwsm session with systemd-cat to log everything to journal

--- src/linux/modules/system/desktop-modules/greetd/greetd.nix
+++ src/linux/modules/system/desktop-modules/greetd/greetd.nix
@@ -66,7 +66,7 @@ args@{
         enable = true;
         settings = rec {
           initial_session = {
-            command = "${pkgs.uwsm}/bin/uwsm start ${self.settings.package}/bin/${self.settings.cmdline}";
+            command = "${pkgs.systemd}/bin/systemd-cat -t uwsm_start ${pkgs.uwsm}/bin/uwsm start ${self.settings.package}/bin/${self.settings.cmdline}";
             user = self.host.mainUser.username;
           };
           default_session = initial_session;
