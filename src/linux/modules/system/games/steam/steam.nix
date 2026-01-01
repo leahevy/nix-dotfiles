187518daf903e87d0c7a4b60020ef986c7142ccd
Leah <leah.nixdev@gmail.com>
2026-01-01 17:48:55 +0100

    Attempt to fix steam for running with STEAM_RUNTIME=0 to fix ldcache problems with 25.11

--- src/linux/modules/system/games/steam/steam.nix
+++ src/linux/modules/system/games/steam/steam.nix
@@ -31,6 +31,21 @@ args@{
       withWayland = steamHomeConfig.withWayland or false;
       dataPath = self.settings.dataPath;
 
+      steamExtraPkgs =
+        pkgs: with pkgs; [
+          pkgsi686Linux.xorg.libXtst
+          pkgsi686Linux.xorg.libXi
+          pkgsi686Linux.xorg.libSM
+          pkgsi686Linux.xorg.libICE
+          pkgsi686Linux.gtk2-x11
+          pkgsi686Linux.pipewire
+          pkgsi686Linux.libpulseaudio
+          pkgsi686Linux.gdk-pixbuf
+          pkgsi686Linux.bzip2
+          pkgsi686Linux.openal
+          pkgsi686Linux.SDL2
+        ];
+
       wrapSteamBinary =
         pkg: binaryName:
         if (dataPath != null) then
@@ -42,7 +57,9 @@ args@{
                 --set XDG_DATA_HOME "${dataPath}/.local/share" \
                 --set XDG_CONFIG_HOME "${dataPath}/.config" \
                 --set XDG_CACHE_HOME "${dataPath}/.cache" \
-                --set XDG_STATE_HOME "${dataPath}/.local/state"
+                --set XDG_STATE_HOME "${dataPath}/.local/state" \
+                --set LD_LIBRARY_PATH "/lib32:/lib" \
+                --set STEAM_RUNTIME "0"
             '';
           })
         else
@@ -73,10 +90,19 @@ args@{
                 XDG_CONFIG_HOME = "${dataPath}/.config";
                 XDG_CACHE_HOME = "${dataPath}/.cache";
                 XDG_STATE_HOME = "${dataPath}/.local/state";
+                LD_LIBRARY_PATH = "/lib32:/lib";
+                STEAM_RUNTIME = "0";
               };
+              extraPkgs = steamExtraPkgs;
             }
           else
-            pkgs.steam;
+            pkgs.steam.override {
+              extraEnv = {
+                LD_LIBRARY_PATH = "/lib32:/lib";
+                STEAM_RUNTIME = "0";
+              };
+              extraPkgs = steamExtraPkgs;
+            };
 
         remotePlay.openFirewall = true;
         dedicatedServer.openFirewall = true;
