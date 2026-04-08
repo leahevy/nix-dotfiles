265034472687055fa14475fc516560fff90dd3af
Leah <leah.nixdev@gmail.com>
2026-04-08 08:53:17 +0200

    Add tor-browser module

---
+++ src/linux/modules/browser/tor.nix
@@ -0,0 +1,37 @@
+args@{
+  lib,
+  pkgs,
+  pkgs-unstable,
+  funcs,
+  helpers,
+  defs,
+  self,
+  ...
+}:
+{
+  name = "tor";
+
+  group = "browser";
+  input = "linux";
+
+  settings = {
+    persistCache = false;
+  };
+
+  on = {
+    linux.home = config: {
+      home.packages = with pkgs; [
+        tor-browser
+      ];
+
+      home.persistence."${self.persist}" = {
+        directories = [
+          ".tor project"
+        ]
+        ++ lib.optionals self.settings.persistCache [
+          ".cache/tor project"
+        ];
+      };
+    };
+  };
+}
