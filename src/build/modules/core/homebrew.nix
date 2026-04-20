113e289907bdc1a591f2bd680af9c5f7814b6d28
Leah <leah.nixdev@gmail.com>
2026-04-20 15:28:54 +0200

    nx brew should consider yes as default

--- src/build/modules/core/homebrew.nix
+++ src/build/modules/core/homebrew.nix
@@ -222,8 +222,8 @@ in
               echo
 
               if [[ "''${1:-}" != "-y" ]]; then
-                read -rp "$(echo -e "''${WHITE}Proceed? [y/N] ''${RESET}")" confirm
-                if [[ ! "$confirm" =~ ^[Yy](es)?$ ]]; then
+                read -rp "$(echo -e "''${WHITE}Proceed? [Y/n] ''${RESET}")" confirm
+                if [[ ! "''${confirm:-yes}" =~ ^[Yy](es)?$ ]]; then
                   echo -e "''${RED}Aborted.''${RESET}"
                   exit 0
                 fi
