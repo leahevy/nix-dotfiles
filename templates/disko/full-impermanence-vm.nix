3eca091148d5afd47e1bf02ffe91c5aaa51a3e56
Leah <leah.nixdev@gmail.com>
2026-04-29 18:58:38 +0200

    Add disko template for VM disk

---
+++ templates/disko/full-impermanence-vm.nix
@@ -0,0 +1,75 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                  "umask=0077"
+                ];
+              };
+            };
+
+            swap = {
+              size = "4G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            };
+
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "@root" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+
+                  "@root-empty" = {
+                    mountpoint = null;
+                    mountOptions = [ ];
+                  };
+
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+
+                  "@persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                };
+                mountpoint = null;
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
