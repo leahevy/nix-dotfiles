5d79da7880538ae912aefe8683b3cc793ee52482
Leah <leah.nixdev@gmail.com>
2026-05-02 21:17:06 +0200

    Fix keybinds of cmp.nvim

--- src/common/nvim-modules/cmp.nix
+++ src/common/nvim-modules/cmp.nix
@@ -137,8 +137,8 @@ args@{
 
                       mapping = lib.mkMerge [
                         {
-                          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-                          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                          "<C-f>" = "cmp.mapping.scroll_docs(-4)";
+                          "<C-t>" = "cmp.mapping.scroll_docs(4)";
                           "<C-Space>" = "cmp.mapping.complete()";
                           "<C-e>" = "cmp.mapping.abort()";
                           "<CR>" = "cmp.mapping.confirm({ select = true })";
