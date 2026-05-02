71ab6c2c69098939c20b1bb98306f81097a0e669
Leah <leah.nixdev@gmail.com>
2026-05-02 16:55:08 +0200

    Disable gamemode and keyd effectivly in any VM

--- src/linux/desktop-modules/gamemode.nix
+++ src/linux/desktop-modules/gamemode.nix
@@ -17,34 +17,36 @@ args@{
   disableOnVM = true;
 
   module = {
-    linux.system = config: {
-      programs.gamemode = {
-        enable = true;
-        settings = {
-          general = {
-            renice = 10;
-          };
-
-          custom = {
-            start = self.notifyUser {
-              inherit pkgs;
-              title = "GameMode";
-              body = "GameMode started";
-              icon = "com.valvesoftware.Steam";
-              urgency = "normal";
-              validation = { inherit config; };
+    linux.system =
+      config:
+      lib.mkIf (!(helpers.resolveFromHost config [ "isVM" ] false)) {
+        programs.gamemode = {
+          enable = true;
+          settings = {
+            general = {
+              renice = 10;
             };
-            end = self.notifyUser {
-              inherit pkgs;
-              title = "GameMode";
-              body = "GameMode ended";
-              icon = "com.valvesoftware.Steam";
-              urgency = "normal";
-              validation = { inherit config; };
+
+            custom = {
+              start = self.notifyUser {
+                inherit pkgs;
+                title = "GameMode";
+                body = "GameMode started";
+                icon = "com.valvesoftware.Steam";
+                urgency = "normal";
+                validation = { inherit config; };
+              };
+              end = self.notifyUser {
+                inherit pkgs;
+                title = "GameMode";
+                body = "GameMode ended";
+                icon = "com.valvesoftware.Steam";
+                urgency = "normal";
+                validation = { inherit config; };
+              };
             };
           };
         };
       };
-    };
   };
 }
