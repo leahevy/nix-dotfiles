b7161889c857d997c07ec81c5ba06f7626eb9d61
Leah <leah.nixdev@gmail.com>
2026-05-06 20:38:07 +0200

    If host.remote settings are configured -> ensure the buildUser is configured to login using the buildPublicSSHKey with ssh

--- src/linux/services/sshd.nix
+++ src/linux/services/sshd.nix
@@ -28,23 +28,34 @@ args@{
   };
 
   module = {
-    linux.system = config: {
-      services = {
-        openssh = {
-          enable = true;
-          ports = [ self.settings.port ];
-          settings = {
-            PasswordAuthentication = false;
-            AllowUsers = [ self.host.mainUser.username ];
-            X11Forwarding = false;
-            PermitRootLogin = "no";
+    linux.system =
+      config:
+      let
+        buildUser = self.host.remote.buildUser;
+        buildPublicSSHKey = self.host.remote.buildPublicSSHKey;
+        hasBuildKey = buildPublicSSHKey != null;
+      in
+      {
+        services = {
+          openssh = {
+            enable = true;
+            ports = [ self.settings.port ];
+            settings = {
+              PasswordAuthentication = false;
+              AllowUsers = lib.unique ([ self.host.mainUser.username ] ++ lib.optional hasBuildKey buildUser);
+              X11Forwarding = false;
+              PermitRootLogin = if hasBuildKey && buildUser == "root" then "prohibit-password" else "no";
+            };
           };
         };
-      };
 
-      # Already configured in system module as ssh keys should always be persisted!
-      environment.persistence.${self.persist} = { };
-    };
+        users.users = lib.mkIf hasBuildKey {
+          ${buildUser}.openssh.authorizedKeys.keys = [ buildPublicSSHKey ];
+        };
+
+        # Already configured in system module as ssh keys should always be persisted!
+        environment.persistence.${self.persist} = { };
+      };
 
     virtual.linux.system = config: {
       virtualisation.vmVariant = {
