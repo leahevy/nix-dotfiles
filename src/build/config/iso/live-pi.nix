2c1b6de8b3a8defeb245f38b5236f8a2898a6cc9
Leah <leah.nixdev@gmail.com>
2026-05-29 18:14:48 +0200

    Fix live-pi build after borgbackup was added: we must disable tests that fail on slow setups like qemu emulation

--- src/build/config/iso/live-pi.nix
+++ src/build/config/iso/live-pi.nix
@@ -13,6 +13,42 @@
     ./live-common.nix
   ];
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      pythonPackagesExtensions = (prev.pythonPackagesExtensions or [ ]) ++ [
+        (pyFinal: pyPrev: {
+          ipython = pyPrev.ipython.overridePythonAttrs (old: {
+            disabledTests = (old.disabledTests or [ ]) ++ [
+              "test_stream_performance"
+            ];
+          });
+          nbmake = pyPrev.nbmake.overridePythonAttrs (old: {
+            disabledTests = (old.disabledTests or [ ]) ++ [
+              "test_when_import_error_then_fails"
+            ];
+          });
+          proxy-py = pyPrev.proxy-py.overridePythonAttrs (old: {
+            disabledTests = (old.disabledTests or [ ]) ++ [
+              "test_empties_queue"
+              "test_subscribe"
+              "test_publish"
+              "test_unsubscribe"
+              "test_event_subscriber"
+            ];
+          });
+          aiohttp = pyPrev.aiohttp.overridePythonAttrs (old: {
+            disabledTests = (old.disabledTests or [ ]) ++ [
+              "test_cookie_pattern_performance"
+              "test_regex_performance"
+              "test_secure_https_proxy_absolute_path[pyloop-http]"
+              "test_secure_https_proxy_absolute_path[pyloop-https]"
+            ];
+          });
+        })
+      ];
+    })
+  ];
+
   sdImage.compressImage = false;
 
   boot.kernelParams = [
