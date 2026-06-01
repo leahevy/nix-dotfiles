c679e6307adc1c221f4fed3f8b802d934cc68351
Leah <leah.nixdev@gmail.com>
2026-06-01 17:26:38 +0200

    Fix proxy settings for syncthing server

--- src/linux/server/syncthing.nix
+++ src/linux/server/syncthing.nix
@@ -233,8 +233,14 @@ args@{
             locations."/" = {
               proxyPass = "http://127.0.0.1:${toString guiPort}";
               proxyWebsockets = true;
+              recommendedProxySettings = false;
               extraConfig = ''
                 proxy_set_header Host "localhost:${toString guiPort}";
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto $scheme;
+                proxy_set_header X-Forwarded-Host $host;
+                proxy_set_header X-Forwarded-Server $hostname;
                 proxy_read_timeout 600s;
                 proxy_send_timeout 600s;
               '';
