574494a4203f179c30bc10563c838000d433971a
Leah <leah.nixdev@gmail.com>
2026-06-03 08:30:16 +0200

    Add fail2ban pushover notification system on IP bans (disabled per default)

--- src/linux/services/fail2ban.nix
+++ src/linux/services/fail2ban.nix
@@ -14,6 +14,14 @@ args@{
   group = "services";
   input = "linux";
 
+  options = {
+    pushoverOnBan = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Send a Pushover notification when fail2ban bans an IP.";
+    };
+  };
+
   module = {
     linux.system = config: {
       services.fail2ban = {
@@ -31,6 +39,48 @@ args@{
       enabled = config: {
         nx.linux.server.healthchecks.requireServicesUp = [ "fail2ban.service" ];
       };
+    };
+
+    ifEnabled.linux.notifications.pushover = {
+      linux.system =
+        { config, pushoverOnBan, ... }:
+        lib.mkIf pushoverOnBan {
+          nx.linux.monitoring.journal-watcher.ignorePatterns = [
+            { service = "fail2ban-pushover.service"; }
+          ];
+
+          systemd.services.fail2ban-pushover = {
+            description = "Pushover notifications for fail2ban ban events";
+            after = [
+              "fail2ban.service"
+              "network-online.target"
+            ];
+            requires = [ "fail2ban.service" ];
+            wants = [ "network-online.target" ];
+            wantedBy = [ "multi-user.target" ];
+            serviceConfig = {
+              Type = "simple";
+              User = "root";
+              Restart = "always";
+              RestartSec = "5";
+              ExecStart = pkgs.writeShellScript "fail2ban-pushover" ''
+                ${pkgs.systemd}/bin/journalctl -u fail2ban.service -f -n 0 --output=cat | \
+                  while IFS= read -r _line; do
+                    if [[ "$_line" =~ NOTICE[[:space:]]+\[([^\]]+)\][[:space:]]Ban[[:space:]]+([^[:space:]]+) ]]; then
+                      _jail="''${BASH_REMATCH[1]}"
+                      _ip="''${BASH_REMATCH[2]}"
+                      ${config.nx.linux.notifications.pushover.send {
+                        title = "Fail2ban";
+                        message = "Banned $_ip in jail $_jail";
+                        shellVars = true;
+                        type = "warn";
+                      }}
+                    fi
+                  done
+              '';
+            };
+          };
+        };
     };
   };
 }
