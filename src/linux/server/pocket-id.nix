0796fcdcb8d62789e462573752e62c652207780e
Leah <leah.nixdev@gmail.com>
2026-06-11 16:48:43 +0200

    Try to fix proxy settings for pocket-id vhost

--- src/linux/server/pocket-id.nix
+++ src/linux/server/pocket-id.nix
@@ -516,10 +516,16 @@ in
             locations."/" = {
               proxyPass = "http://127.0.0.1:${toString port}";
               proxyWebsockets = true;
+              recommendedProxySettings = false;
               extraConfig = ''
                 proxy_set_header Host $host;
                 proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
                 proxy_set_header X-Forwarded-Proto $scheme;
+                proxy_set_header X-Forwarded-Host $host;
+                proxy_set_header X-Forwarded-Server $hostname;
+                proxy_read_timeout 600s;
+                proxy_send_timeout 600s;
               '';
             };
           };
