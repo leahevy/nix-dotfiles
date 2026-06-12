ace4d37030e978279c1bf0c214c68ce7f9e7a3a9
Leah <leah.nixdev@gmail.com>
2026-06-12 21:57:24 +0200

    Add missing ipv6 allowed samba hosts

--- src/linux/server/samba.nix
+++ src/linux/server/samba.nix
@@ -84,9 +84,12 @@ args@{
           allowedHosts = lib.concatStringsSep " " (
             [
               "127.0.0.1"
+              "::1"
               "192.168.0.0/16"
               "10.0.0.0/8"
               "172.16.0.0/12"
+              "fe80::/10"
+              "fc00::/7"
             ]
             ++ additionalAllowedHosts
           );
