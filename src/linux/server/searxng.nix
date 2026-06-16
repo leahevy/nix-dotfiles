633fe0a0cdaa47139ee60f4302d188936289e862
Leah <leah.nixdev@gmail.com>
2026-06-16 08:26:20 +0200

    Fix searxng proxy settings #2

--- src/linux/server/searxng.nix
+++ src/linux/server/searxng.nix
@@ -378,7 +378,14 @@ args@{
             };
             locations."/autocompleter" = {
               proxyPass = "http://127.0.0.1:${toString port}";
-              extraConfig = corsExtraConfig;
+              recommendedProxySettings = false;
+              extraConfig = ''
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto $scheme;
+              ''
+              + corsExtraConfig;
             };
             locations."/" = {
               proxyPass = "http://127.0.0.1:${toString port}";
