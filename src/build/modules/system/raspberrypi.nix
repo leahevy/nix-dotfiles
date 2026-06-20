af085e6dd828d304db4ad74e2e8bc5e223e95a4f
Leah <leah.nixdev@gmail.com>
2026-06-20 21:04:04 +0200

    Ignore past events on CPU throttled healthcheck (raspberry-pi only)

--- src/build/modules/system/raspberrypi.nix
+++ src/build/modules/system/raspberrypi.nix
@@ -101,7 +101,7 @@ args@{
             _hex=$(printf '%s' "$_out" | ${pkgs.gnused}/bin/sed 's/throttled=//')
             if [[ "$_hex" =~ ^0x[0-9A-Fa-f]+$ ]]; then
               _dec=$(( _hex ))
-              if [[ $(( _dec & 0xF000F )) -ne 0 ]]; then
+              if [[ $(( _dec & 0xF )) -ne 0 ]]; then
                 printf '%s\n' "$_hex" >&3
               fi
               if [[ $(( _dec & 0xF )) -ne 0 ]]; then
