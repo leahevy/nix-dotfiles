c4fdf8ff13cd56639d4ef2ab5339a1486573ef4e
Leah <leah.nixdev@gmail.com>
2026-06-21 09:11:23 +0200

    Add raspberry-pi flake overlays

--- src/build/config/system/raspberrypi.nix
+++ src/build/config/system/raspberrypi.nix
@@ -8,13 +8,21 @@
 }:
 { config, lib, ... }:
 {
-  imports = with inputs.nixos-raspberrypi.nixosModules; [
-    raspberry-pi-5.base
-    raspberry-pi-5.page-size-16k
-    raspberry-pi-5.display-vc4
-    raspberry-pi-5.bluetooth
-    trusted-nix-caches
-  ];
+  imports =
+    with inputs.nixos-raspberrypi.nixosModules;
+    [
+      raspberry-pi-5.base
+      raspberry-pi-5.page-size-16k
+      raspberry-pi-5.display-vc4
+      raspberry-pi-5.bluetooth
+      trusted-nix-caches
+    ]
+    ++ (
+      if host.settings.system.desktop == null then
+        [ inputs.nixos-raspberrypi.lib.inject-overlays-global ]
+      else
+        [ inputs.nixos-raspberrypi.lib.inject-overlays ]
+    );
 
   nixpkgs.overlays = allOverlays;
   nixpkgs.config.allowUnfreePredicate = unfreePredicate;
