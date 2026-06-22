c14c2ab36127b451455caf4ab291f3dd453c920c
Leah <leah.nixdev@gmail.com>
2026-06-22 15:22:45 +0200

    Set fixed lower CPUQuota for builds on a raspberry-pi

--- src/build/modules/system/raspberrypi.nix
+++ src/build/modules/system/raspberrypi.nix
@@ -18,6 +18,8 @@ args@{
       nix.settings.max-jobs = 1;
       nix.settings.cores = 2;
 
+      systemd.services.nix-daemon.serviceConfig.CPUQuota = "150%";
+
       boot.kernelParams = [
         "nvme_core.default_ps_max_latency_us=0"
         "pcie_aspm=off"
