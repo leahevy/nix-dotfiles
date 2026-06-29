43c3e5c10aaef4c732005550cf19d31403580321
Leah <leah.nixdev@gmail.com>
2026-06-29 08:06:26 +0200

    Configure performance cpuFreqGovernor for raspberry-pi

--- src/build/modules/system/raspberrypi.nix
+++ src/build/modules/system/raspberrypi.nix
@@ -16,6 +16,8 @@ args@{
 
   module = {
     linux.system = config: {
+      powerManagement.cpuFreqGovernor = "performance";
+
       boot.kernelParams = [
         "nvme_core.default_ps_max_latency_us=0"
         "pcie_aspm=off"
