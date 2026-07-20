005a5c04e5b130afee9829f0c1627a2ef58f7614
Leah <leah.nixdev@gmail.com>
2026-07-20 16:24:33 +0200

    Show branch for status.sh if branch is not main

--- scripts/deployment/status.sh
+++ scripts/deployment/status.sh
@@ -7,7 +7,10 @@ parse_git_args "$@"
 
 if [[ "$ONLY_CONFIG" != true ]]; then
 	cd "$NXCORE_DIR"
-	echo -e "${GREEN}Status of core repository ${WHITE}(.config/nx/nxcore)${RESET}..."
+	CORE_BRANCH="$(git branch --show-current)"
+	CORE_BRANCH_SUFFIX=""
+	[[ -n "$CORE_BRANCH" && "$CORE_BRANCH" != "main" ]] && CORE_BRANCH_SUFFIX=" ${YELLOW}(${CORE_BRANCH})${RESET}"
+	echo -e "${GREEN}Status of core repository ${WHITE}(.config/nx/nxcore)${RESET}${CORE_BRANCH_SUFFIX}..."
 	if [[ ${#EXTRA_ARGS[@]} -gt 0 ]]; then
 		git status --porcelain "${EXTRA_ARGS[@]}"
 	else
@@ -19,7 +22,10 @@ if [[ "$ONLY_CORE" != true ]] && [[ -d "$CONFIG_DIR/.git" ]]; then
 	if [[ "$ONLY_CONFIG" != true ]]; then
 		echo
 	fi
-	echo -e "${GREEN}Status of config repository ${WHITE}(.config/nx/nxconfig)${RESET}..."
+	CONFIG_BRANCH="$(cd "$CONFIG_DIR" && git branch --show-current)"
+	CONFIG_BRANCH_SUFFIX=""
+	[[ -n "$CONFIG_BRANCH" && "$CONFIG_BRANCH" != "main" ]] && CONFIG_BRANCH_SUFFIX=" ${YELLOW}(${CONFIG_BRANCH})${RESET}"
+	echo -e "${GREEN}Status of config repository ${WHITE}(.config/nx/nxconfig)${RESET}${CONFIG_BRANCH_SUFFIX}..."
 	if [[ ${#EXTRA_ARGS[@]} -gt 0 ]]; then
 		(cd "$CONFIG_DIR" && git status --porcelain "${EXTRA_ARGS[@]}")
 	else
