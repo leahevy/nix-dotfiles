f14a4c04879a4400297c51abd53eb5c3840dd4c7
Leah <leah.nixdev@gmail.com>
2026-07-28 17:33:03 +0200

    Add journal watcher ignore the second pam_rssh message in case of error - the more detailed error is kept

--- src/linux/security/rssh-sudo.nix
+++ src/linux/security/rssh-sudo.nix
@@ -19,6 +19,15 @@ args@{
   ];
 
   module = {
+    enabled = config: {
+      nx.linux.monitoring.journal-watcher.ignorePatterns = [
+        {
+          tag = "pam_rssh";
+          string = "None of these keys passed authentication";
+        }
+      ];
+    };
+
     linux.system = config: {
       security.pam.rssh.enable = true;
       security.pam.services.sudo.rssh = true;
