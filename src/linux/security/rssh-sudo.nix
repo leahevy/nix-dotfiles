8ce4b36c8e3c0f11dcf7117b78dcd7d64c7263c5
Leah <leah.nixdev@gmail.com>
2026-08-01 13:50:20 +0200

    Try to fix rssh reload in on-going tmux sessions

--- src/linux/security/rssh-sudo.nix
+++ src/linux/security/rssh-sudo.nix
@@ -32,5 +32,21 @@ args@{
       security.pam.rssh.enable = true;
       security.pam.services.sudo.rssh = true;
     };
+
+    ifEnabled.common.shell.fish.home = config: {
+      home.file."${config.xdg.configHome}/fish-init/10-ssh-auth-sock.fish".text = ''
+        function __nx_refresh_ssh_auth_sock --on-event fish_prompt
+          if test -z "$TMUX"; or test -z "$SSH_AUTH_SOCK"; or test -S "$SSH_AUTH_SOCK"
+            return
+          end
+
+          set -l sock (command tmux show-environment SSH_AUTH_SOCK 2>/dev/null | string replace -r '^SSH_AUTH_SOCK=' "")
+
+          if test -S "$sock"
+            set -gx SSH_AUTH_SOCK $sock
+          end
+        end
+      '';
+    };
   };
 }
