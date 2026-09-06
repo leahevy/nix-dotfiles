35ee4e87602995b97d4e94a70cdc44a24df0dcf6
Leah <leah.nixdev@gmail.com>
2026-09-06 09:21:19 +0200

    Do not run borg repository checks if the backup was skipped

--- src/linux/storage/borg-backup.nix
+++ src/linux/storage/borg-backup.nix
@@ -952,7 +952,7 @@ args@{
               ${pkgs.coreutils}/bin/echo "All snapshots deleted successfully"
             fi
             ${pkgs.coreutils}/bin/rm -f /tmp/nx-backup-skipped
-            if [ "$exitStatus" -eq 0 ]; then
+            if [ "$exitStatus" -eq 0 ] && [ ! -f "/tmp/nx-backup-no-success" ]; then
               ${pkgs.borgbackup}/bin/borg check --repository-only --verbose --max-duration=${toString self.settings.repoCheckMaxDuration}
             fi
           '';
