afe2755932b523ee7449cd0d059b2d6f75ccd210
Leah <leah.nixdev@gmail.com>
2026-09-06 11:57:11 +0200

    Fix navidrome proxy settings

--- src/linux/server/navidrome.nix
+++ src/linux/server/navidrome.nix
@@ -131,8 +131,12 @@ args@{
             locations."/" = {
               proxyPass = "http://127.0.0.1:${toString port}";
               proxyWebsockets = true;
+              recommendedProxySettings = false;
               extraConfig = ''
                 proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto $scheme;
               '';
             };
           };
