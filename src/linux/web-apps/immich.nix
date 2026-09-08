2edfcd1c9d64eaaf3cd771b1607739862a0884d2
Leah <leah.nixdev@gmail.com>
2026-09-08 11:14:31 +0200

    Update immich web-app to support auto discovery of servers

--- src/linux/web-apps/immich.nix
+++ src/linux/web-apps/immich.nix
@@ -7,78 +7,122 @@ args@{
   self,
   ...
 }:
+let
+  autoHosts = lib.filterAttrs (
+    _: hostCfg: (hostCfg.remote.exposedServices.immich or false) != false
+  ) (self.nixOSHosts or { });
+in
 {
   name = "immich";
 
   group = "web-apps";
   input = "linux";
 
-  settings = {
-    name = "Immich";
-    webapp = "immich";
-    categories = [
-      "Photography"
-      "Graphics"
-    ];
-    protocol = "https";
-    subdomain = "";
-    args = "";
-    domain =
-      if self ? user && self.user.isStandalone then
-        self.user.homeserverDomain
-      else if self ? host then
-        self.host.homeserverDomain
-      else
-        null;
-  };
-
-  submodules = {
-    linux = {
-      desktop-modules = {
-        web-app = true;
-      };
+  options = {
+    additionalHosts = lib.mkOption {
+      type = lib.types.attrsOf (
+        lib.types.submodule {
+          options = {
+            subdomain = lib.mkOption {
+              type = lib.types.str;
+            };
+            domain = lib.mkOption {
+              type = lib.types.str;
+            };
+          };
+        }
+      );
+      default = { };
+      description = "Additional immich instances to include alongside auto-discovered ones.";
     };
   };
 
-  assertions = [
-    {
-      assertion = self.settings.subdomain != null && self.settings.subdomain != "";
-      message = "Subdomain required to be set!";
-    }
-    {
-      assertion = self.settings.domain != null && self.settings.domain != "";
-      message = "Domain required to be set!";
-    }
-  ];
+  submodules = {
+    linux.desktop-modules.web-app = true;
+  };
 
   module = {
     linux.enabled =
       config:
-      lib.mkIf (config.nx.linux.desktop-modules.web-app.buildWebApp != null) {
-        nx.linux.desktop.niri.autoTiler.ignoredAppIds =
-          (config.nx.linux.desktop-modules.web-app.buildWebApp {
-            webapp = "immich";
-            subdomain = config.nx.linux.web-apps.immich.subdomain;
-            domain = config.nx.linux.web-apps.immich.domain;
-            protocol = "https";
-            args = config.nx.linux.web-apps.immich.args;
-          }).appIds;
+      let
+        buildFn = config.nx.linux.desktop-modules.web-app.buildWebApp;
+        fromAutoHosts = lib.mapAttrsToList (
+          profileName: hostCfg:
+          let
+            val = hostCfg.remote.exposedServices.immich;
+            subdomain = if val == true then "photos" else val;
+            domain = hostCfg.remote.baseDomain or (hostCfg.remote.address or null);
+          in
+          {
+            label = hostCfg.hostname or profileName;
+            inherit subdomain domain;
+          }
+        ) autoHosts;
+        fromAddlHosts = lib.mapAttrsToList (name: host: {
+          label = name;
+          inherit (host) subdomain domain;
+        }) config.nx.linux.web-apps.immich.additionalHosts;
+        allHosts = fromAutoHosts ++ fromAddlHosts;
+        multiple = lib.length allHosts > 1;
+        allSettings = lib.imap0 (i: host: {
+          webapp = if multiple then "immich-${toString i}" else "immich";
+          inherit (host) subdomain domain;
+          protocol = "https";
+          args = "";
+        }) allHosts;
+      in
+      lib.mkIf (buildFn != null && allHosts != [ ]) {
+        nx.linux.desktop.niri.autoTiler.ignoredAppIds = lib.concatMap (s: (buildFn s).appIds) allSettings;
       };
 
     linux.home =
-      config:
+      { config, additionalHosts, ... }:
       let
         iconPath = "${helpers.packageFile args config.nx.linux.desktop-modules.web-app.dashboardIcons
           "svg/immich.svg"
         }";
-        webAppSettings = self.settings // {
+
+        fromAutoHosts = lib.mapAttrsToList (
+          profileName: hostCfg:
+          let
+            val = hostCfg.remote.exposedServices.immich;
+            subdomain = if val == true then "photos" else val;
+            domain = hostCfg.remote.baseDomain or (hostCfg.remote.address or null);
+          in
+          {
+            label = hostCfg.hostname or profileName;
+            inherit subdomain domain;
+          }
+        ) autoHosts;
+
+        fromAdditionalHosts = lib.mapAttrsToList (name: host: {
+          label = name;
+          inherit (host) subdomain domain;
+        }) additionalHosts;
+
+        allHosts = fromAutoHosts ++ fromAdditionalHosts;
+        multiple = lib.length allHosts > 1;
+
+        allSettings = lib.imap0 (i: host: {
+          name = if multiple then "Immich (${host.label})" else "Immich";
+          webapp = if multiple then "immich-${toString i}" else "immich";
           inherit iconPath;
-        };
+          inherit (host) subdomain domain;
+          categories = [
+            "Photography"
+            "Graphics"
+          ];
+          protocol = "https";
+          args = "";
+        }) allHosts;
       in
       {
-        home.file = (config.nx.linux.desktop-modules.web-app.buildWebApp webAppSettings).homeFiles;
-        xdg.desktopEntries =
-          (config.nx.linux.desktop-modules.web-app.buildWebApp webAppSettings).desktopEntries;
+        home.file = lib.mkMerge (
+          map (s: (config.nx.linux.desktop-modules.web-app.buildWebApp s).homeFiles) allSettings
+        );
+        xdg.desktopEntries = lib.mkMerge (
+          map (s: (config.nx.linux.desktop-modules.web-app.buildWebApp s).desktopEntries) allSettings
+        );
       };
   };
 }
