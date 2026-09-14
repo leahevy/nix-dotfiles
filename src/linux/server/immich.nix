793ca9a54dc6899718ee145ac637f7d49c5cde38
Leah <leah.nixdev@gmail.com>
2026-09-14 07:19:24 +0200

    Immich frame fine tunings #9

--- src/linux/server/immich.nix
+++ src/linux/server/immich.nix
@@ -154,7 +154,7 @@ args@{
 
           clockTextShadow = lib.mkOption {
             type = lib.types.str;
-            default = "0 0 3px rgba(0,0,0,1), 0 2px 9px rgba(0,0,0,1), 0 0 3rem rgba(0,0,0,1), 0 0 6rem rgba(0,0,0,0.8)";
+            default = "0 0 6px black, 0 0 12px black, 0 0 20px black, 0 0 30px black, 0 0 40px black";
             description = "CSS text-shadow value applied to the clock widget to ensure readability without a background.";
           };
 
